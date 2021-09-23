192b4a3279da5f52ff026c690ec95f43f573856d
Muhammad Adnan <79781004+Adnan-Swati@users.noreply.github.com>
2021-09-23 10:21:10 -0700

    Add files via upload

---
+++ dartpractice4.dart
@@ -0,0 +1,170 @@
+void main() {
+// List a = ['adnan',23,true];
+//   print (a);
+//   var lst1 = [1,2,3,4,5,6,7,8,9];
+//   var a1 = lst1.length;
+//   print (a1);
+//    var lst1 = [1,2,3,4,5,6,7,8,9];
+//   var a1 = lst1.first;
+//   print (a1);
+//    var lst1 = [1,2,3,4,5,6,7,8,9];
+//   var a1 = lst1.last;
+//   print (a1);
+//    var lst1 = [1,2,3,4,5,6,7,8,9];
+//   var a1 = lst1.elementAt(2);
+//   print (a1);
+//     var lst1 = [1,2,3,4,5,6,7,8,9];
+//   var a1 = lst1.elementAt(2);
+//   print ('there are $a1 in the element');
+//    var lst1 = [1,2,3,4,5,6,7,8,9];
+//   var a1 = lst1.elementAt(2);
+//   print ('there are $lst1 element in the list');
+//    var lst1 = [1,2,3,4,5,6,7,8,9];
+//   var a2 = lst1.length;
+//   var a3 = lst1.first;
+//   var a4 = lst1.last;
+//   var a5 = lst1.elementAt(4);
+//   var a6 = lst1.length-1;
+//   var a1 = lst1.elementAt(2);
+//   print ('there are $lst1 element in the list');
+//   print ( 'There are elements 9 in $a2');
+//   print ( 'There are elements 9 in $a3');
+//   print ( 'There are elements 9 in $a4');
+//   print ( 'There are elements 9 in $a5');
+//   print ( 'There are elements 9 in $a6');
+//   print ( 'There are elements 9 in $a1');
+  
+//   var lst1 = [1,2,3,4,5,6,7,8,9];
+//   var a2 = lst1.length;
+//   var a3 = lst1.first;
+//   var a4 = lst1.last;
+//   var a5 = lst1.elementAt(4);
+//   var a6 = lst1.length-2;
+//   var a1 = lst1.elementAt(2);
+//   print ('there are $lst1 element in the list');
+//   print ( 'There are elements 9 in $a2');
+//   print ( 'There are elements 9 in $a3');
+//   print ( 'There are elements 9 in $a4');
+//   print ( 'There are elements 9 in $a5');
+//   print ( 'There are elements 9 in $a6');
+//   print ( 'There are elements 9 in $a1');
+//   var lst1 = [1,2,3,4,5,6,7,8,9];
+//   var a2 = lst1.length;
+//   var a3 = lst1.first;
+//   var a4 = lst1.last;
+//   var a5 = lst1.elementAt(4);
+//   var a6 = lst1.length-2;
+//   var a1 = lst1.elementAt(2);
+//   print ('there are $lst1 element in the list');
+//   print ( 'There are elements 9 in $a2');
+//   print ( 'There are elements 9 in $a3');
+//   print ( 'There are elements 9 in $a4');
+//   print ( 'There are elements 9 in $a5');
+//   print ( 'There are elements 9 in $a6');
+//   print ( 'There are elements 9 in $a1');
+//   var lst1 = [1,2,3,4,5,6,7,8,9];
+//   lst1[1] = 4;
+//   print (lst1);
+// //   var a1 = [ 'adnan','zeeshan',true,false,25];
+//   a1[1] = 'yasir';
+//   print (a1);
+//    var a2 = [ 'adnan','zeeshan',true,false,25];
+//   a2 [4] = 24;
+//   print (a2);
+//    var a3 = [ 'adnan','zeeshan',true,false,25];
+//   a3[3] = 5;
+//   print (a3);
+//     var a1 = [ 'adnan','zeeshan',true,false,25];
+//  a1.replaceRange(2,4,[3,4]);
+//   print (a1);
+//     var a2 = [ 'adnan','zeeshan',true,false,25];
+// a2.replaceRange( 0,3,['zeeshan,yasir']);
+//   print(a2);
+//     var a3 = [ 'adnan','zeeshan',true,false,25];
+//   a3.replaceRange( 0,4,[5,6,7,8]);
+//   print (a3);
+//     var a4 = [ 'adnan','zeeshan',true,false,25];
+//   a4.replaceRange( 2,5,['new 2','3 and 4','adnan']);
+//   print(a4);
+  
+//   var lst1 = [1,2,3,4,5,6,7,8,9];
+//   lst1.sort();
+//   print(lst1);
+//   var lst2 = [3,6,7,1,8,5,9,2,4];
+//   lst2.sort();
+//   print (lst2);
+//   var lst1 = [ 'adnan','zeeshan','yasir','sheraz'];
+//   lst1.sort();
+//   print (lst1);
+//   var integer = [ 1,2,4,7,9,8,6,5,4];
+//   integer.sort();
+//   print(integer);
+//   List abc = ['adnan','zeeshan','mumtaz','yasir','sheraz'];
+//   abc.sort();
+//   print(abc);
+//   List abc = ['adnan','zeeshan'];
+//   abc.add('zeeshan');
+//   abc.add('mumtaz');
+//   print(abc);
+//   if(abc.isEmpty){
+//     print('list is empty');
+//   }else{
+//     print ('list is not empty');
+//   }
+//   if (abc.isNotEmpty){
+//     print ('list is not empty');
+//   }else{
+//     print ('list is empty');
+//   }
+//   abc.clear();
+//   print(abc);
+  
+  
+//   var a = [1,2,3,4,5,6,7,8,9];
+// //   a.clear();
+//   a.add(10);
+//   a.add(11);
+//   print (a);
+//   var a = [1,2,3,4,5,6,7,8,9];
+//   a.clear();
+//    a.add(10);
+//    a.add(11);
+//   if(a.isEmpty){
+//     print ('list is empty');
+//   }
+//   print (a);
+//   List aba = ['adnan','zeeshan'];
+//   if(aba.isEmpty){
+//     print ('list is empty');
+//   }else{
+//     print ('list is not empty');
+//   }
+//   var abc = [ 'a','b','c','d','e','f'];
+//   var reversed = List.of(abc.reversed);
+//   print(reversed);
+//   var reversed = List.of(abc.reversed);
+//   var reversed = List.of(abc.reversed);
+//   print (reversed);
+//   var abc = [ 'a','b','c','d','e','f'];
+//   abc.add('5');
+//   abc.addAll(['1','2','3','4']);
+//   print (abc);
+//    var abc = [1,2,3,4,5,6,7,8];
+// abc.insert(3,0);
+//   print (abc);
+//      var abc = [1,2,3,4,5,6,7,8];
+// abc.insertAll(3,[2,3,4]);
+//   print (abc);
+//      var abc = ['adnan','zeeshan','yasir'];
+//  abc.remove(2);
+//   abc.removeAt(abc.length-2);
+//   abc.removeLast();
+//   abc.clear();
+//   var ab = abc.length;
+//   abc.removeAt(abc.length-1);
+//   print (abc);
+//   print (abc);
+//      var abc = [1,2,3,4,5,6,7,8,9];
+// abc.removeWhere((i) =>i<5);
+//   print (abc);
+  }
